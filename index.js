298ea5c3ec5bfde07d3e8b8f801b83228c186d2d
Joseph Martire <joemart06@gmail.com>
2020-08-25 17:14:04 -0400

    Can't find path, print files in frontend/ directory

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const fs = require('fs')
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static("/frontend/build"))
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"))
-        let files = fs.readdirSync(path.resolve(__dirname, "/frontend/"))
+        res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
+        let files = fs.readdirSync(path.resolve(__dirname, "frontend/"))
         console.log("my files are " + files)
     })
     
