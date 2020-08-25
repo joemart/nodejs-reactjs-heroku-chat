a32659dbab05ab28b15e5c1246b205b293b7df95
Joseph Martire <joemart06@gmail.com>
2020-08-25 13:50:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+})
+
+app.listen(port)
