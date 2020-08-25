39810dd52390f0e88e6c93ec803ee4d39bdaa8be
Joseph Martire <joemart06@gmail.com>
2020-08-25 14:47:46 -0400

    Eliminate Procfile, add heroku-postbuild

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express()
 const path = require('path')
 const port = 5000
 
-app.use(express.static("frontend/build"))
-app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-})
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static("frontend/build"))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    })
+}
+
 
 app.listen(process.env.PORT || port)
