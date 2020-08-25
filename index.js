e1c6dec4d3f2343d6625f29d75968bcdbc7b481e
Joseph Martire <joemart06@gmail.com>
2020-08-25 17:35:24 -0400

    Can't find path, use path.join

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = 5000
 const fs = require('fs')
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("frontend/build"))
+    app.use(express.static(path.join(__dirname, "frontend", "build")))
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, "index.html"))
-        let files = fs.readdirSync(__dirname)
+        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+        let files = fs.readdirSync(path.join(__dirname, "frontend"))
         console.log("my files are " + files)
     })
     
