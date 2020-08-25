66975e3f5f7dd535a0574e632ffa7e356981b1a4
Joseph Martire <joemart06@gmail.com>
2020-08-25 17:59:39 -0400

    Finally, submodule was cached

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 5000
-const fs = require('fs')
+
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "frontend", "build")))
     app.get('*', (req,res)=>{
         res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
-        let files = fs.readdirSync(path.join(__dirname, ".."))
-        console.log("my files are " + files)
     })
     
 }
