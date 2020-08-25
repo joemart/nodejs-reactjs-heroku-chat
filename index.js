64088d110c9ff79b39512813111ca117804eabf8
Joseph Martire <joemart06@gmail.com>
2020-08-25 14:09:05 -0400

    Add path.resolve

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const app = express()
 const path = require('path')
 const port = 5000
 
-// app.use(express.static(path.join(__dirname, "frontend", "build")))
-// app.get('/', (req,res)=>{
-//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
-// })
-app.get('/', (req,res)=>{
-    res.send("HELLO")
+app.use(express.static("frontend/build"))
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 })
 
 app.listen(process.env.PORT || port)
