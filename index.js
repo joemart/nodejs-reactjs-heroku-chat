0931befe550f10f51d74ac2359c39108d63e5fd1
Joseph Martire <joemart06@gmail.com>
2020-08-26 15:51:34 -0400

    Add socket.io variable to test error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 5000
+const io = require('socket.io')
 
 
 if(process.env.NODE_ENV === 'production'){
