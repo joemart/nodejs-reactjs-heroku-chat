0a05cea479292fc831a563289fa1ac0a7f1b7c6e
Joseph Martire <joemart06@gmail.com>
2020-08-26 16:02:05 -0400

    Add socket events and users variable

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const http = require('http').createServer(app)
 const io = require('socket.io').listen(http)
 
 
+let users = {}
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "frontend", "build")))
     app.get('*', (req,res)=>{
